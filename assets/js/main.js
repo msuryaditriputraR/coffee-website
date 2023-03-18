17bf238ff9648e8f8f2282015ff686ea428bfb64
Muh Suryadi Triputra Rahman <msuryaditriputra@gmail.com>
2023-03-18 17:28:45 +0800

    feat: scroll header

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,14 @@
 /*=============== CHANGE BACKGROUND HEADER ===============*/
+const scrollHeader = () => {
+    document
+        .getElementById("header")
+        .classList[this.scrollY >= 50 ? "add" : "remove"]("scroll-header");
+};
 
+window.addEventListener("scroll", scrollHeader);
 
-/*=============== SHOW SCROLL UP ===============*/ 
-
+/*=============== SHOW SCROLL UP ===============*/
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
 
-
 /*=============== SCROLL REVEAL ANIMATION ===============*/
-
